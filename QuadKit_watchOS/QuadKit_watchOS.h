5c63082ac970e94d18dde65dc91153335b41f2fe
Gabriel O'Flaherty-Chan <gabriel@shopify.com>
2017-02-18 22:00:25 +0900

    add watches framework

---
+++ QuadKit_watchOS/QuadKit_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  QuadKit_watchOS.h
+//  QuadKit_watchOS
+//
+//  Created by Gabriel O'Flaherty-Chan on 2017-02-18.
+//  Copyright © 2017 Gabrieloc. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for QuadKit_watchOS.
+FOUNDATION_EXPORT double QuadKit_watchOSVersionNumber;
+
+//! Project version string for QuadKit_watchOS.
+FOUNDATION_EXPORT const unsigned char QuadKit_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuadKit_watchOS/PublicHeader.h>
+
+
